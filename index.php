f5b9118b8a25c50ac9fa82afe810d22075ec6bf8
KozaMiklos <kozamiki2000@gmail.com>
2023-10-03 20:18:22 +0200

    Kezdeti oldal felépítés készen van. Logó, és social gombok illetve testimonial rész majdnem kész.

---
+++ index.php
@@ -0,0 +1,381 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../PHP_beadando/index.css">
+    
+</head>
+<body>
+<div class="grid-container">
+
+<!-- navbar  -->
+<nav class="navbar">
+    <div class="navbar-logo">
+        <img class="logo-nav" src="../PHP_beadando/img/logo.png" alt="logó helye">
+    </div>
+<div class="navbar-elements">
+<p>Programok</p>
+<p>Ételek</p>
+<p>Plusz kényelmi szolgáltatások</p>
+<p>Szobák</p>
+<p>Contact</p>
+</div>
+</nav>
+<!-- navbar end -->
+
+<!-- Hero section -->
+<div class="hero">
+
+<div class="hero-graphic">
+    <h1>Hévízi-tó</h1>
+    <img src="" alt="kép1">
+    <img src="" alt="kép2">
+    <img src="" alt="kép3">
+</div>
+
+<div class="hero-description">
+    <p>Gyertek el a .... hotelbe hogy Magyarország egyik legszebb helyén legyetek</p>
+    <button>Foglald le a szobádat már ma!</button>
+</div>
+
+</div>
+<!-- hero end -->
+
+
+<!-- programs -->
+<section class="programs">
+<h1>programok</h1>
+    <div class="programs-list">
+        <p>Keszthelyi Festetics-kastély túra</p>
+    </div>
+
+    <div class="programs-list">
+        <p>Túra Hévízi-tó környékén Tavirózsa tanösvény, és annak környéke  </p>
+    </div>
+
+    <div class="programs-list">
+        <p>Golf imádóknak Zala Springs Golf Resort</p>
+    </div>
+</section>
+<!-- programs end -->
+
+<!-- foods -->
+<section class="food">
+
+    <div class="food-welcome"> 
+    <h2 class="food-welcome-header-text">A hotel vendégeinek biztosítunk</h2>
+
+    <p>Magyarország legjobb séfei által elkészült ételeket ehetsz, 
+        amit gondosan helyi gazdáktól beszerzett alapanyagokat
+        tartalmaznak mindenki számára</p>
+
+    </div>
+
+    <div class="breakfast-img"> <img src="" alt="ide jön még hat kép egy mappával együtt"></div>
+    <div class="breakfast-text"> <p>ide jön a reggelinek a szövege</p>   </div>
+    <div class="lunch-img"> <img src="" alt="ide jön még hat kép egy mappával együtt"></div>
+    <div class="lunch-text"><p>ide jön a ebédnek a szövege</p></div>    
+    <div class="dinner-img"> <img src="" alt="ide jön még hat kép egy mappával együtt"></div>
+    <div class="dinner-text"><p>ide jön a vacsorának a szövege</p></div>
+</section>
+<!-- foods end -->
+
+<!-- convenience section -->
+<section class="convenience-container">
+
+<div class="pool-container">
+    <img class="pool-picture1"  src="" alt="kép1">
+    <img class="pool-picture2" src="" alt="kép2">
+    <img class="pool-picture3" src="" alt="kép3">
+    <img class="pool-picture4" src="" alt="kép4">
+    <img class="pool-picture5" src="" alt="kép5">
+</div>
+
+<div class="convenience-text"><p>A vendégeink tudnak választani szauna és medencézés között</p></div>
+
+<div class="sauna-container">
+    <img class="sauna-picture1" src="" alt="kép1">
+    <img class="sauna-picture2" src="" alt="kép2">
+    <img class="sauna-picture3" src="" alt="kép3">
+    <img class="sauna-picture4" src="" alt="kép4">
+    <img class="sauna-picture5" src="" alt="kép5">
+</div>
+</section>
+<!-- convenience section end -->
+
+<!-- Testimonials section -->
+<section class="testimonials-container">
+    <div class="testimonial-text">
+        <h2>Nem csak mi állítjuk hogy kiváló szolgáltatást biztosítunk, 
+        lássa meg mások véleményét is!</h2>
+    </div>
+
+    <div class="testimonials-card-container">
+
+    <div class="testimonials-card">
+        <img class="card-img" src="../PHP_beadando/img/kovacs_istvan.jpg" alt="Kovács István">
+        <img class="stars" src="../PHP_beadando/img/5star.png" alt="5 star rating">
+        <p>Ember véleményszövege</p>
+        <p>Kovács István</p>
+    </div>
+
+    <div class="testimonials-card">
+        <img class="card-img" src="../PHP_beadando/img/horvath_maria.jpg" alt="Horváth Mária">
+        <img class="stars" src="../PHP_beadando/img/4,5star.png" alt="4.5 star rating">
+        <p>Ember véleményszövege</p>
+        <p>Horváth Mária</p>
+    </div>
+
+    <div class="testimonials-card">
+        <img class="card-img" src="../PHP_beadando/img/Nagy_peter.jpg" alt="kép az emberről">
+        <img class="stars" src="../PHP_beadando/img/5star.png" alt="5 star rating">
+        <p>Ember véleményszövege</p>
+        <p>Nagy Péter</p>
+    </div>
+
+    <div class="testimonials-card">
+        <img class="card-img" src="../PHP_beadando/img/szabo_panna.jpg" alt="kép az emberről">
+        <img class="stars" src="../PHP_beadando/img/5star.png" alt="5 star rating">
+        <p>Ember véleményszövege</p>
+        <p>Szabó Anna</p>
+    </div>
+
+    <div class="testimonials-card">
+        <img class="card-img" src="../PHP_beadando/img/toth_janos.jpg" alt="kép az emberről">
+        <img class="stars" src="../PHP_beadando/img/4,5star.png" alt="4.5 star rating">
+        <p>Ember véleményszövege </p>
+        <p>Tóth János</p>
+    </div>
+    </div>
+</section>
+<!-- Testimonials section end -->
+
+<!-- CTA section -->
+<section class="CTA">
+    <div class="rooms-card">
+        <img class="room-img" src="../PHP_beadando/img/hotel-room.jpg" alt="szoba képe">
+        <p class="room-convenience">Mi jár a szobához 
+            (szauna vagy medence vagy milyen túra) 
+        </p>
+
+        <div class="wifi_and_parking">
+            <div class="wifi">
+                <p>Lesz egy icon még</p>
+                <p>Van wifi vagy sem</p>
+            </div>
+
+            <div class="parking">
+                <p>Lesz egy icon még</p>
+                <p>Van wifi vagy sem</p>
+            </div>
+        </div>
+
+        <div class="meals_and_capacity">
+            <div class="meals">
+                <p>Lesz egy icon még</p>
+                <p>Milyen étkezés van</p>
+            </div>
+
+            <div class="capacity">
+                <p>Lesz egy icon még</p>
+                <p>Hány fős a szoba</p>
+            </div>
+        </div>
+
+        <div class="price_size">
+            <div class="price">
+                <p>Lesz egy icon még</p>
+                <p>Ára a szobának</p>
+            </div>
+
+            <div class="size">
+                <p>Lesz egy icon még</p>
+                <p>Szoba nagysága négyzetméterben</p>
+            </div>
+        </div>
+
+        <div class="order">
+            <button class="order-btn">Lefoglalom</button>
+        </div>
+    </div>
+
+    <div class="rooms-card">
+        <img class="room-img" src="../PHP_beadando/img/hotel-room.jpg" alt="szoba képe">
+        <p class="room-convenience">Mi jár a szobához 
+            (szauna vagy medence vagy milyen túra) 
+        </p>
+
+        <div class="wifi_and_parking">
+            <div class="wifi">
+                <p>Lesz egy icon még</p>
+                <p>Van wifi vagy sem</p>
+            </div>
+
+            <div class="parking">
+                <p>Lesz egy icon még</p>
+                <p>Van wifi vagy sem</p>
+            </div>
+        </div>
+
+        <div class="meals_and_capacity">
+            <div class="meals">
+                <p>Lesz egy icon még</p>
+                <p>Milyen étkezés van</p>
+            </div>
+
+            <div class="capacity">
+                <p>Lesz egy icon még</p>
+                <p>Hány fős a szoba</p>
+            </div>
+        </div>
+
+        <div class="price_size">
+            <div class="price">
+                <p>Lesz egy icon még</p>
+                <p>Ára a szobának</p>
+            </div>
+
+            <div class="size">
+                <p>Lesz egy icon még</p>
+                <p>Szoba nagysága négyzetméterben</p>
+            </div>
+        </div>
+
+        <div class="order">
+            <button class="order-btn">Lefoglalom</button>
+        </div>
+    </div>
+
+    <div class="rooms-card">
+        <img class="room-img" src="../PHP_beadando/img/hotel-room.jpg" alt="szoba képe">
+        <p class="room-convenience">Mi jár a szobához 
+            (szauna vagy medence vagy milyen túra) 
+        </p>
+
+        <div class="wifi_and_parking">
+            <div class="wifi">
+                <p>Lesz egy icon még</p>
+                <p>Van wifi vagy sem</p>
+            </div>
+
+            <div class="parking">
+                <p>Lesz egy icon még</p>
+                <p>Van wifi vagy sem</p>
+            </div>
+        </div>
+
+        <div class="meals_and_capacity">
+            <div class="meals">
+                <p>Lesz egy icon még</p>
+                <p>Milyen étkezés van</p>
+            </div>
+
+            <div class="capacity">
+                <p>Lesz egy icon még</p>
+                <p>Hány fős a szoba</p>
+            </div>
+        </div>
+
+        <div class="price_size">
+            <div class="price">
+                <p>Lesz egy icon még</p>
+                <p>Ára a szobának</p>
+            </div>
+
+            <div class="size">
+                <p>Lesz egy icon még</p>
+                <p>Szoba nagysága négyzetméterben</p>
+            </div>
+        </div>
+
+        <div class="order">
+            <button class="order-btn">Lefoglalom</button>
+        </div>
+    </div>
+
+
+    <div class="rooms-card">
+        <img class="room-img" src="../PHP_beadando/img/hotel-room.jpg" alt="szoba képe">
+        <p class="room-convenience">Mi jár a szobához 
+            (szauna vagy medence vagy milyen túra) 
+        </p>
+
+        <div class="wifi_and_parking">
+            <div class="wifi">
+                <p>Lesz egy icon még</p>
+                <p>Van wifi vagy sem</p>
+            </div>
+
+            <div class="parking">
+                <p>Lesz egy icon még</p>
+                <p>Van wifi vagy sem</p>
+            </div>
+        </div>
+
+        <div class="meals_and_capacity">
+            <div class="meals">
+                <p>Lesz egy icon még</p>
+                <p>Milyen étkezés van</p>
+            </div>
+
+            <div class="capacity">
+                <p>Lesz egy icon még</p>
+                <p>Hány fős a szoba</p>
+            </div>
+        </div>
+
+        <div class="price_size">
+            <div class="price">
+                <p>Lesz egy icon még</p>
+                <p>Ára a szobának</p>
+            </div>
+
+            <div class="size">
+                <p>Lesz egy icon még</p>
+                <p>Szoba nagysága négyzetméterben</p>
+            </div>
+        </div>
+
+        <div class="order">
+            <button class="order-btn">Lefoglalom</button>
+        </div>
+    </div>
+
+
+    
+    
+   
+</section>
+<!-- CTA section end -->
+
+<footer class="footer">
+    <div class="socials">
+        <img class="logo-icon" src="../PHP_beadando/img/logo.png" alt="logó helye">
+
+        <div class="social-medias">
+            <img class="social-icons" src="../PHP_beadando/img/logo-facebook.svg" alt="facebook">
+            <img class="social-icons" src="../PHP_beadando/img/logo-instagram.svg" alt="Instagram">
+            <img class="social-icons" src="../PHP_beadando/img/logo-tiktok.svg" alt="Tiktok">
+        </div>
+    </div>
+
+    <div class="contact">
+        <h3>Akadt még valamivel kérdésed? Na habozz minket keresni az alábbi elérhetőségeinken!</h3>
+        <p>ikon: 0680404050</p>
+        <p>ikon: ugyfelszolgalat@telekom.hu</p>
+    </div>
+
+    <div class="map">
+        <p>Ide egy igényes térkép fog kerülni</p>
+    </div>
+</footer>
+
+
+
+
+
+</div>
+</body>
+</html>
